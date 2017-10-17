89cfa30bb04936931eeabd6be0f8a929c07b4ae2
Liam <liam@fusionspim.com>
2017-10-17 13:19:21 +0100

    Fix folder creation on bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,17 +4,15 @@ include __DIR__ . '/../vendor/autoload.php';
 define('TEST_FIXTURE_DIR', __DIR__ . '/fixtures/');
 define('TEST_OUTPUT_DIR', __DIR__ . '/output/');
 
-function initializeOutputFolders()
-{
-    $paths = [
-        TEST_OUTPUT_DIR . 'markdown/',
-    ];
+$paths = [
+    TEST_OUTPUT_DIR,
+    TEST_OUTPUT_DIR . 'markdown/',
+];
 
-    foreach ($paths as $path) {
-        if (! file_exists($path)) {
-            mkdir($path, 0777, true);
-        } else {
-            array_map('unlink', glob($path . '*.*'));
-        }
+foreach ($paths as $path) {
+    if (! file_exists($path)) {
+        mkdir($path, 0777, true);
+    } else {
+        array_map('unlink', glob($path . '*.*'));
     }
 }
