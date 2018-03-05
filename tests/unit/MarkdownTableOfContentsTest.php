7856b216bb0cf18f230499c17b5f3f3db43827cb
Wes Hooper <weshooper@users.noreply.github.com>
2018-03-05 15:19:56 +0000

    Simplify/fix tests

--- tests/unit/MarkdownTableOfContentsTest.php
+++ tests/unit/MarkdownTableOfContentsTest.php
@@ -8,17 +8,9 @@ class MarkdownTableOfContentsTest extends TestCase
 {
     public function testProcess()
     {
-        $actualMarkdown   = file_get_contents(TEST_FIXTURE_DIR . 'markdown/sample.md');
-        $expectedMarkdown = file_get_contents(TEST_FIXTURE_DIR . 'markdown/sample-toc.md');
-
-        file_put_contents(
-            TEST_OUTPUT_DIR . 'markdown/sample-toc.md',
-            (new MarkdownTableOfContents($actualMarkdown))->process()
-        );
-
         $this->assertEquals(
-            $expectedMarkdown,
-            file_get_contents(TEST_OUTPUT_DIR . 'markdown/sample-toc.md')
+            file_get_contents(__DIR__ . '/../fixtures/sample-toc.md'),
+            (new MarkdownTableOfContents(file_get_contents(__DIR__ . '/../fixtures/sample.md')))->process()
         );
     }
 }
